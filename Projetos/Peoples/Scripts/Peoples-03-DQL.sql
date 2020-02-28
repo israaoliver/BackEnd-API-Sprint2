66534cc34123ddf686a4ab517cdf53c5b5c25138
israaoliver <isrraaoliveira@gmail.com>
2020-02-28 17:24:44 -0300

    bd

--- Projetos/Peoples/Scripts/Peoples-03-DQL.sql
+++ Projetos/Peoples/Scripts/Peoples-03-DQL.sql
@@ -8,3 +8,6 @@ SELECT CONCAT (Nome,' ',Sobrenome) FROM Funcionarios
 SELECT * FROM TiposUsuarios;
 
 SELECT * FROM Usuarios;
+
+SELECT Usuarios.IdUsuario,Usuarios.IdTipoUsuario, Usuarios.Email, Usuarios.Senha, TiposUsuarios.Titulo 
+FROM Usuarios INNER JOIN TiposUsuarios ON TiposUsuarios.IdTipoUsuario = Usuarios.IdTipoUsuario;
